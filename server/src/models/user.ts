2937853733727c97ae0cb8cedc178b05b16779e1
Heet Bhuva <heetbhuva1405@gmail.com>
2025-09-22 22:31:40 +0530

    add : userSchema

---
+++ server/src/models/user.ts
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            minLength: 2,
+        },
+        lastName: {
+            type: String,
+            minLength: 2,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            immutable: true,
+            trim: true,
+        },
+        hashedPassword: {
+            required: true,
+            type: String,
+        },
+        role: {
+            type: String,
+            enum: ['owner', 'customer'],
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const User = mongoose.model('user', userSchema);
+module.exports = User;
