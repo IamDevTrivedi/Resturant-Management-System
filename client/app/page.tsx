51fbef746dbd28b0cf0d95c3f11f6727f3a590f9
Heet Bhuva <heetbhuva1405@gmail.com>
2025-11-02 01:26:03 +0530

    Hero Section ADDED

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,14 @@
 import { DraggableCard } from '@/components/DraggableCard';
 import { FAQs } from '@/components/FAQs';
 import { Feature } from '@/components/Feature';
+import HeroSection from '@/components/hero-section';
 import { Separator } from '@/components/ui/separator';
 
 export default function Page() {
     return (
         <>
             <div className="mb-8 text-transparent"> JUST FOR MAKING MARGIN </div>
-
+            <HeroSection></HeroSection>
             <Feature />
             <Separator />
 
