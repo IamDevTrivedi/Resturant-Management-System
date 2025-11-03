8c11e6f2e9ed91c02c717e75a427e5b5de3e822b
Heet Bhuva <heetbhuva1405@gmail.com>
2025-11-04 03:16:38 +0530

    Dashboard page created

---
+++ client/app/restaurant/layout.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { LoadingPage } from "@/components/Loading";
+import { useUserData } from "@/store/user";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+
+    const {user}=useUserData()
+    const router=useRouter()
+    useEffect(()=>{
+        if(!user){
+            router.replace('/login')
+        }
+        if(!user||user.role=='customer'){
+            router.replace('/home')
+          }
+    },[])
+
+    if(!user||user.role=='customer'){
+        return <LoadingPage/>;
+      }
+
+  
+    return <>
+    {children}
+    </>;
+}
