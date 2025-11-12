b269cc2bddc6d3df6c7f0f054f57e7bb62d4bed9
IamDevTrivedi <devtrivedi22082005@gmail.com>
2025-11-12 17:56:16 +0530

    ADD: Implement booking model with schema for user and restaurant bookings

---
+++ server/src/models/booking.ts
@@ -0,0 +1,55 @@
+import mongoose, { InferSchemaType } from 'mongoose';
+const { Schema } = mongoose;
+
+const bookingSchema = new Schema({
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true,
+    },
+
+    restaurantID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'restaurant',
+        required: true,
+    },
+
+    bookingAt: {
+        type: Date,
+        required: true,
+    },
+
+    numberOfGuests: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+
+    message: {
+        type: String,
+        trim: true,
+        default: '',
+    },
+
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'confirmed', 'rejected'],
+        default: 'pending',
+    },
+
+    category: {
+        type: String,
+        enum: ['breakfast', 'lunch', 'dinner'],
+        default: 'other',
+    },
+
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+});
+
+const Booking = mongoose.model('booking', bookingSchema);
+export type IBooking = mongoose.Document & InferSchemaType<typeof bookingSchema>;
+
+export default Booking;
