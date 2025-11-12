20bedef6c8586122e5467691bc55b15eb449720b
Jenish230-daiict <202301172@daiict.ac.in>
2025-11-13 02:17:39 +0530

    fix: Update background gradient class in Logo component for consistency

--- client/components/Logo.tsx
+++ client/components/Logo.tsx
@@ -4,13 +4,13 @@ import React from 'react';
 
 export function Logo() {
     return (
-        <div className="flex h-10 w-10 p-0.5 items-center justify-center rounded-xl bg-gradient-to-br from-primary to-primary/50 border-2 border-primary shadow-lg shadow-primary/20">
+        <div className="flex h-10 w-10 p-0.5 items-center justify-center rounded-xl bg-linear-to-r from-primary to-primary/50 border-2 border-primary shadow-lg shadow-primary/20">
              <Image
-                src="/logo1.png"        // ✅ image inside /public folder
+                src="/logo1.png"        
                 alt="Company Logo"
                 width={50}
                 height={50}
-                className="rounded-md" // optional styling
+                className="rounded-md" 
             />
         </div>
     );
