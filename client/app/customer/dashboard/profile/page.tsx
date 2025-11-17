b019f2711e03012a12ccfe0fe3a8746dc8da3950
Jenish230-daiict <202301172@daiict.ac.in>
2025-11-18 00:24:48 +0530

    feat: add user profile and edit profile pages with fetching functionality

---
+++ client/app/customer/dashboard/profile/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { backend } from "@/config/backend";
+import { Toast } from "@/components/Toast";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
+
+interface UserProfile {
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+  cityName: string;
+}
+
+export default function ProfilePage() {
+  const [profile, setProfile] = useState<UserProfile | null>(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  // Fetch profile
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const { data } = await backend.get("/api/v1/auth/my-profile");
+        if (data?.success) {
+          setProfile(data.data);
+        } else {
+          Toast.error("Failed to load profile");
+        }
+      } catch (error) {
+        console.error(error);
+        Toast.error("Error loading profile");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-background">
+        <Loader2 className="animate-spin w-6 h-6 text-primary" />
+      </div>
+    );
+  }
+
+  if (!profile) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p>Failed to load profile</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex justify-center py-15 px-4">
+      <Card className="w-full max-w-lg">
+        <CardHeader>
+          <CardTitle className="text-2xl font-semibold">My Profile</CardTitle>
+        </CardHeader>
+
+        <CardContent className="space-y-4">
+          <ProfileRow label="First Name" value={profile.firstName} />
+          <ProfileRow label="Last Name" value={profile.lastName} />
+          <ProfileRow label="Email" value={profile.email} />
+          <ProfileRow label="City" value={profile.cityName} />
+        </CardContent>
+
+        <div className="flex justify-around p-4 w-full">
+
+            {/* <Button onClick={() => router.push("/dashboard/profile/edit")}>
+            Reset Password
+          </Button> */}
+          <Button 
+            className="cursor-pointer"
+            onClick={() => router.push("/customer/dashboard/profile/edit")}>
+            Edit Profile
+          </Button>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+function ProfileRow({ label, value }: { label: string; value: string }) {
+  return (
+    <div className="flex justify-between border-b py-2">
+      <span className="font-medium">{label}</span>
+      <span className="text-muted-foreground">{value || "-"}</span>
+    </div>
+  );
+}
